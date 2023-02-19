35a1876dce89f7405cd3da4d0b6e8053f5665bac
anusha <anu636238@gmail.com>
2023-02-19 15:35:57 +0530

    two sum

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Approach 1 :-
+
 class Solution
 {
 public:
@@ -9,11 +12,40 @@ public:
         for (int i = 0; i < nums.size(); i++) // i=2
         {
             if (mp.find(target - nums[i]) == mp.end()) // 6-4=2
-                mp[nums[i]] = i;                       //
+                mp[nums[i]] = i;
             else
-                return {mp[target - nums[i]], i}; //
+                return {mp[target - nums[i]], i};
         }
 
         return {-1, -1}; // tc: O(n)   sc:O(n)
+    }
+};
+
+// Approach 2 :-
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) // 2     7    11      15
+        {                                     // i j
+            int sum = 0;
+            for (int j = 0; j < nums.size(); j++) // i=0 j=2
+            {
+                if (i != j) // 2!=15
+                {
+                    sum = nums[i] + nums[j]; //  sum=2+11 =13
+                    if (sum == target)       //
+                    {
+                        ans.push_back(i); // 2
+                        // ans.push_back(j);
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
