4734580425cc63bbcd17a79854459f3c6e4f712c
anusha <anu636238@gmail.com>
2023-03-01 14:40:51 +0530

    Binary Tree Right Side View

---
+++ 199. Binary Tree Right Side View.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// TC : O(N)   SC : O(N)
+
+class Solution
+{
+public:
+    void traversal(TreeNode *root, int level, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+        if (ans.size() == level)
+            ans.push_back(root->val);
+        traversal(root->right, level + 1, ans);
+        traversal(root->left, level + 1, ans);
+    }
+    vector<int> rightSideView(TreeNode *root)
+    {
+        vector<int> ans;
+        traversal(root, 0, ans);
+        return ans;
+    }
+};
