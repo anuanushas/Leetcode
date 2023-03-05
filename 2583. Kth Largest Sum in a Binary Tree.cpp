c427f9e16c5bbbd48fa7839c7112137fa235e181
anusha <anu636238@gmail.com>
2023-03-05 14:48:44 +0530

    kth largest sum in BT

---
+++ 2583. Kth Largest Sum in a Binary Tree.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    long long kthLargestLevelSum(TreeNode *root, int k)
+    {
+        vector<long long> ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size = q.size();
+            long long sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+                sum = sum + node->val;
+            }
+            ans.push_back(sum);
+        }
+        sort(ans.begin(), ans.end());
+        if (ans.size() < k)
+            return -1;
+        return ans[ans.size() - k];
+    }
+};
